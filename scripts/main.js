1f321d18f5cb0b1343d66c4fe57f78c4eb8d8c7d
cbennet <cbennet@wpi.edu>
2022-07-03 12:08:18 -0400

    add scroll + fix resize

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var blocklyArea = document.getElementById('blocklyArea');
+// var blocklyArea = document.getElementById('blocklyArea');
 var blocklyDiv = document.getElementById('blocklyDiv');
 var workspace = Blockly.inject(blocklyDiv,
     {
@@ -17,19 +17,12 @@ var workspace = Blockly.inject(blocklyDiv,
     });
 var onresize = function (e) {
     // Compute the absolute coordinates and dimensions of blocklyArea.
-    var element = blocklyArea;
     var x = 0;
     var y = 0;
-    do {
-        x += element.offsetLeft;
-        y += element.offsetTop;
-        element = element.offsetParent;
-    } while (element);
-    // Position blocklyDiv over blocklyArea.
     blocklyDiv.style.left = x + 'px';
     blocklyDiv.style.top = y + 'px';
-    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
-    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+    blocklyDiv.style.width = (window.innerWidth/3*2) + 'px';
+    blocklyDiv.style.height = window.innerHeight + 'px';
     Blockly.svgResize(workspace);
 };
 window.addEventListener('resize', onresize, false);
